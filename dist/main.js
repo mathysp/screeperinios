be0b9474f79c05f903d6269fafa8bda67cb0a006
Pieter Mathys <pieter.mathys@dazzle.be>
2016-09-22 23:55:39 +0200

    Added upgrader

--- dist/main.js
+++ dist/main.js
@@ -13,7 +13,7 @@ module.exports.loop = function () {
     }
   }
 
-  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester')
   console.log('Harvesters: ' + harvesters.length);
 
   if(harvesters.length < 2) {
@@ -21,7 +21,7 @@ module.exports.loop = function () {
     console.log('Spawning new harvester: ' + newName);
   }
 
-  var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+  var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder')
   console.log('Builders: ' + builders.length);
 
   if(builders.length < 1) {
@@ -29,11 +29,19 @@ module.exports.loop = function () {
     console.log('Spawning new Builder: ' + newName);
   }
 
+  var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader')
+  console.log('Upgraders: ' + upgraders.length);
+
+  if(upgraders.length < 1) {
+    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+    console.log('Spawning new Upgrader: ' + newName);
+  }
+
   var tower = Game.getObjectById('507f86d4d161584d2c854c19');
   if(tower) {
     var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
           filter: (structure) => structure.hits < structure.hitsMax
-  });
+    })
     if(closestDamagedStructure) {
       tower.repair(closestDamagedStructure);
     }
--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -10,12 +10,12 @@ var roleHarvester = {
     }
     else {
       var targets = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (structure.structureType == STRUCTURE_EXTENSION ||
-                  structure.structureType == STRUCTURE_SPAWN ||
-                  structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-        }
-      });
+                filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION ||
+              structure.structureType == STRUCTURE_SPAWN ||
+              structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+    }
+    });
       if(targets.length > 0) {
         if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
           creep.moveTo(targets[0]);
