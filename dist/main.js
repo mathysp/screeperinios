12d71788ed096e1bed3c9e7cf67a25ead284a1ee
Pieter Mathys <pieter.mathys@dazzle.be>
2016-09-22 23:05:28 +0200

    Init dist

---
+++ dist/main.js
@@ -0,0 +1,57 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+
+  for(var name in Memory.creeps) {
+    if(!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log('Clearing non-existing creep memory:', name);
+    }
+  }
+
+  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  console.log('Harvesters: ' + harvesters.length);
+
+  if(harvesters.length < 2) {
+    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+    console.log('Spawning new harvester: ' + newName);
+  }
+
+  var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+  console.log('Builders: ' + builders.length);
+
+  if(builders.length < 1) {
+    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
+    console.log('Spawning new Builder: ' + newName);
+  }
+
+  var tower = Game.getObjectById('507f86d4d161584d2c854c19');
+  if(tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+          filter: (structure) => structure.hits < structure.hitsMax
+  });
+    if(closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
+    }
+
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if(closestHostile) {
+      tower.attack(closestHostile);
+    }
+  }
+
+  for(var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if(creep.memory.role == 'harvester') {
+      roleHarvester.run(creep);
+    }
+    if(creep.memory.role == 'upgrader') {
+      roleUpgrader.run(creep);
+    }
+    if(creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
+    }
+  }
+}
