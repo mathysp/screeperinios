2fe069d02824d58a76ded6bf0c3b54c556fa4bd7
Pieter Mathys <pieter.mathys@dazzle.be>
2016-09-24 16:14:02 +0200

    Init rewrite branch

--- dist/main.js
+++ dist/main.js
@@ -1,120 +1,13 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var performRoles = require('performRoles');
+var spawner = require('spawner');
+var countType = require('countType');
+var factory = require('factory');
 
+factory.init();
+factory.run();
 
-// Unit Role Object
-function Unitrole(name, desiredNumber) {
-  this.name = name;
-  this.desired = desiredNumber;
-}
+spawner.spawnNextInQue();
 
+factory.buildArmyWhileIdle();
 
-
-
-/*
- ResourceManager
- */
-
-var mineableTiles = 0;
-
-// get the current Resources
-for(var name in Game.rooms) {
-  //console.log(name);
-  var MeinRoom = Game.rooms[name];
-  //console.log(MeinRoom);
-
-  //get sources
-  var sources = MeinRoom.find(FIND_SOURCES);
-  for(var source in sources) {
-    var sauce = sources[source];
-    var sourceTiles = MeinRoom.lookAtArea(sauce.pos.y-1, sauce.pos.x-1, sauce.pos.y+1, sauce.pos.x+1, true);
-
-    sourceTilesFiltered = sourceTiles.filter(function (el) {
-      return el.type == 'terrain' &&
-          el.terrain == 'plain';
-    });
-
-    mineableTiles += sourceTilesFiltered.length;
-
-    sourceTilesFiltered.forEach(function(sourceTile) {
-      //console.log(sourceTile.type)
-    });
-
-  }
-
-  // Get an idea of the resource income
-
-  // Spend the income
-
-
-}
-
-
-// Roles object
-var roles = {
-  builder: new Unitrole('builder', 3),
-  harvester: new Unitrole('harvester', mineableTiles*2), //@TODO: Rework *2 into rangefinding algorythm
-  upgrader: new Unitrole('upgrader', 3)
-};
-
-
-
-
-
-module.exports.loop = function () {
-
-  // Memory Clearing
-  for(var name in Memory.creeps) {
-    if(!Game.creeps[name]) {
-      delete Memory.creeps[name];
-      console.log('Clearing non-existing creep memory:', name);
-    }
-  }
-
-  for(var role in roles) {
-    var units = _.filter(Game.creeps, (creep) => creep.memory.role == roles[role].name)
-    //console.log(roles[role].name + '; Current: ' + units.length + ', Desired: ' + roles[role].desired);
-
-    if(units.length < roles[role].desired) {
-      var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: roles[role].name});
-      console.log('Spawning new : ' + roles[role].name + ' ' + newName);
-    }
-  }
-
-
-
-
-
-
-
-/*  var tower = Game.getObjectById('507f86d4d161584d2c854c19');
-  if(tower) {
-    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: (structure) => structure.hits < structure.hitsMax
-    })
-    if(closestDamagedStructure) {
-      tower.repair(closestDamagedStructure);
-    }
-
-    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if(closestHostile) {
-      tower.attack(closestHostile);
-    }
-  }*/
-
-  for(var name in Game.creeps) {
-    var creep = Game.creeps[name];
-    if(creep.memory.role == 'harvester') {
-      roleHarvester.run(creep);
-    }
-    if(creep.memory.role == 'upgrader') {
-      roleUpgrader.run(creep);
-    }
-    if(creep.memory.role == 'builder') {
-      roleBuilder.run(creep);
-    }
-  }
-
-
-}
+performRoles(Game.creeps);
