ff71cf26422119c5ac903213fdd6a88d8f9e4473
Pieter Mathys <pieter.mathys@dazzle.be>
2016-09-24 16:10:31 +0200

    Updated harvester logic

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -3,13 +3,35 @@ var roleHarvester = {
   /** @param {Creep} creep **/
   run: function(creep) {
 
+    /*
+    Screep actions: idle, moving; harvesting, transfering
+     */
+ /*   if(creep.memory.action && creep.carry.energy == 0) {
+      creep.memory.action = false;
+      creep.say('harvesting');
+    }
+    if(!creep.memory.action && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.action = true;
+      creep.say('building');
+    }
+
+*/
 
 
     if(creep.carry.energy < creep.carryCapacity) {
       var sources = creep.room.find(FIND_SOURCES);
-      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+
+      var randOffset = Math.floor(Math.random() * 4) + 1;
+      if(randOffset == 1) {
+        if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(sources[1]);
+        }
+      } else {
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(sources[0]);
+        }
       }
+
 
      /* var target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
       if(target) {
