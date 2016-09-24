a04283655facdabcb4a63aaa7a5c6dea3bade8d9
Pieter Mathys <pieter.mathys@dazzle.be>
2016-09-24 23:25:54 +0200

    Updated builder

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -38,14 +38,13 @@ var builder = {
       }
 
       // Go build stuff
-      var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-      if(target) {
-        if(!creep.pos.isNearTo(target))
-          creep.moveTo(target);
-
-        creep.build(target);
-        return;
+      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+      if(targets.length) {
+        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0]);
+        }
       }
+      
     } else {
       var storages = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
